2981046d88db4bafd8f99e8219353c95daf32659
Студент НИУ ВШЭ <student@BASM-402-STD20.staff.corp.local>
2017-09-19 11:56:43 +0300

    мат1сем

---
+++ shtuka.py
@@ -0,0 +1,225 @@
+Python 3.3.5 (v3.3.5:62cf4e77f785, Mar  9 2014, 10:37:12) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 12+4
+16
+>>> 
+>>> 81/9
+9.0
+>>> (986+34-843)/24
+7.375
+>>> 8.56-3.798*9
+-25.622
+>>> x=10
+>>> x = 10
+>>> x
+10
+>>> y = 20
+>>> y
+20
+>>> z=x+y
+>>> z
+30
+>>> mult=x*y
+>>> mult
+200
+>>> 130%30
+10
+>>> 4*30
+120
+>>> 130-4*30
+10
+>>> x=123
+>>> x
+123
+>>> x=10
+>>> x
+10
+>>> type(x)
+<class 'int'>
+>>> y=123/10
+>>> y
+12.3
+>>> type(y)
+<class 'float'>
+>>> z=int(y)
+>>> z
+12
+>>> float(x)
+10.0
+>>> int(13.78)
+13
+>>> word='hello!'
+>>> word
+'hello!'
+>>> type(word)
+<class 'str'>
+>>> my_world='world'
+>>> word+my_world
+'hello!world'
+>>> 13
+13
+>>> greeting=word+''+my_world
+>>> greeting
+'hello!world'
+>>> greeting=word+' '+my_world
+>>> greeting
+'hello! world'
+>>> new greeting='hello!'+', '+'world'
+SyntaxError: invalid syntax
+>>> new_greeting='hello'+', '+'Zhenya'
+>>> new greeting
+SyntaxError: invalid syntax
+>>> new_greeting
+'hello, Zhenya'
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+1000
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+1000
+10
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+1000
+y= 1000
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+1000
+y= 1000
+x= 10
+y= 1000
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+1000
+y= 1000
+x= 10 , y= 1000
+>>> num_str="3"
+>>> num_str
+'3'
+>>> "3"+"5"
+'35'
+>>> "a"*3
+'aaa'
+>>> 'a'*5
+'aaaaa'
+>>> 'a'*5+'!'*2
+'aaaaa!!'
+>>> num=int(num_str)
+>>> num
+3
+>>> type(num)
+<class 'int'>
+>>> int('hello')
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    int('hello')
+ValueError: invalid literal for int() with base 10: 'hello'
+>>> 
+>>> 3/0
+Traceback (most recent call last):
+  File "<pyshell#55>", line 1, in <module>
+    3/0
+ZeroDivisionError: division by zero
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Traceback (most recent call last):
+  File "C:/Users/student/Desktop/mehmeh/urokk.py", line 7, in <module>
+    print("y=",y)
+NameError: name 'y' is not defined
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+y= 1000
+x= 10 , y= 1000
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Как вас зовут? Женя
+hello Женя
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Как вас зовут? Варвара фон Бульон
+Привет Варвара фон Бульон
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Как вас зовут? Paul McCartney
+Привет Paul McCartney
+<class 'str'>
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Traceback (most recent call last):
+  File "C:/Users/student/Desktop/mehmeh/urokk.py", line 9, in <module>
+    human_name=input(x,y)
+NameError: name 'x' is not defined
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Traceback (most recent call last):
+  File "C:/Users/student/Desktop/mehmeh/urokk.py", line 3, in <module>
+    x
+NameError: name 'x' is not defined
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Traceback (most recent call last):
+  File "C:/Users/student/Desktop/mehmeh/urokk.py", line 3, in <module>
+    x
+NameError: name 'x' is not defined
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Вводите: 4
+Вводите у: 3
+4 3 <class 'str'> <class 'str'>
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Вводите: 4
+Вводите у: 3
+4 3 <class 'str'> <class 'str'>
+12
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Вводите: 4
+Вводите у: 3
+4 3 <class 'int'> <class 'str'>
+12
+3333
+>>> ================================ RESTART ================================
+>>> 
+Hello!
+Вводите: 4
+Вводите у: 3
+4 3 <class 'int'> <class 'int'>
+12
+12
+>>> ================================ RESTART ================================
+>>> 
+Вводите: 7
+Ваше число больше пяти
+>>> ================================ RESTART ================================
+>>> 
+Вводите: 3
+>>> ================================ RESTART ================================
+>>> 
+Вводите: 65
+Ваше число больше пяти
+!!
+>>> 
---
+++ urokk.py
@@ -0,0 +1,18 @@
+#print ("Hello!")
+  #умножаю на сто
+
+#print("y=",y)
+#print ("x=",x, ", y=",y)
+
+number=int(input("Вводите: "))
+#y=int(input("Вводите у: "))
+#print(number,y,type(number),type(y))
+#num1=int(number)
+#num2=int(y)
+#print(num1*num2)
+#print(number*y)
+
+if number>5:
+    print("Ваше число больше пяти")
+    print("!!")
+
